ef85c541803dbd45a9752ec0abadbfd16e7f6140
Mayans87 <112621138+Mayans87@users.noreply.github.com>
2022-11-01 11:39:56 +0530

    Updates

---
+++ precache-manifest.ea089f049cdfac938becdb8867832a3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fb52c7708b2fa269c07",
+    "url": "/TextUtils-React/static/css/main.266f5f5e.chunk.css"
+  },
+  {
+    "revision": "9fb52c7708b2fa269c07",
+    "url": "/TextUtils-React/static/js/main.9fb52c77.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/TextUtils-React/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/TextUtils-React/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "fb119b557547dc903a9f",
+    "url": "/TextUtils-React/static/js/runtime~main.fb119b55.js"
+  },
+  {
+    "revision": "ae6e081862c882295ebb135cab7d4266",
+    "url": "/TextUtils-React/index.html"
+  }
+];
